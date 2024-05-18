7b6eff99dfb375c23b464a175240074d8f72e20d
zapapp3 <zapapp3@gmail.com>
2024-05-18 13:03:27 -0700

    set width, height and coordinates

--- app.py
+++ app.py
@@ -10,7 +10,22 @@ class MainFrame(ttk.Frame):
     def __init__(self, master):
         super().__init__(master, padding=10)
         self.pack(fill=BOTH, expand=True)
-        self.default_text()
+        
+        # create form inputs
+        name = self.input_field("Name")
+        email = self.input_field("Email ID")
+        
+    def form():
+        pass
+    
+    def input_field(self, title):
+        container = ttk.Frame(master=self)
+        label = ttk.Label(master=container, text=title)
+        entry = ttk.Entry(master=container)
+        label.pack(side=LEFT)
+        entry.pack(side=RIGHT)
+        container.pack(fill=BOTH, expand=True)
+        return entry
         
     def default_text(self):
         container = ttk.Frame(self)
@@ -22,5 +37,18 @@ class MainFrame(ttk.Frame):
         
 if (__name__ == "__main__"):
     main = ttk.Window("IX - file renamer", themename="darkly")
+    # settings
+    screen_width = main.winfo_screenwidth()
+    screen_height = main.winfo_screenheight()
+    
+    frame_width = round(0.70 * screen_width)
+    frame_height = round(0.80 * screen_height)
+    x = round((screen_width - frame_width) / 2)
+    y = round((screen_height - frame_height) / 2)
+    print(x, y)
+    
+    # main.geometry("{}x{}+{}+{}".format(frame_width, frame_height, x, y))
+    main.geometry("{}x{}+{}+{}".format(frame_width, frame_height, x, y))
+    
     app = MainFrame(main)
     main.mainloop()
