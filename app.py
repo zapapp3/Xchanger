f21fa4b665d5b9ee3ea58f3b9143cf6bba9890be
zapapp3 <zapapp3@gmail.com>
2024-05-18 12:35:29 -0700

    create class MainFrame

--- app.py
+++ app.py
@@ -5,3 +5,22 @@ import shutil
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+
+class MainFrame(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master, padding=10)
+        self.pack(fill=BOTH, expand=True)
+        self.default_text()
+        
+    def default_text(self):
+        container = ttk.Frame(self)
+        label = ttk.Label(master=container, text="This is a simple sample text")
+        label.pack()
+        container.pack(fill=BOTH, expand=True)
+        
+        
+        
+if (__name__ == "__main__"):
+    main = ttk.Window("IX - file renamer", themename="darkly")
+    app = MainFrame(main)
+    main.mainloop()
