a4b4bef2aa48b6d43033af96e31990cee89603ed
zapapp3 <zapapp3@gmail.com>
2024-05-19 01:52:04 -0700

    add file menu to object with dummy methods

--- app.py
+++ app.py
@@ -21,13 +21,14 @@ class MainFrame(ttk.Frame):
         # screen variables
         self.screen_width = main.winfo_screenwidth()
         self.screen_height = main.winfo_screenheight()
-        
         self.frame_width = round(0.70 * self.screen_width)
         self.frame_height = round(0.80 * self.screen_height)
         self.x = round((self.screen_width - self.frame_width) / 2)
         self.y = round((self.screen_height - self.frame_height) / 2)
         
+        # setting screen display
         self.set_display()
+        self.create_menu()
         
     def form(self):
         name = self.input_field("Name")
@@ -42,8 +43,25 @@ class MainFrame(ttk.Frame):
         container.pack(fill=BOTH, expand=True, pady=7)
         return entry
     
+    
+    # dialog methods
+    def open_dialog(self):
+        print("opening the file dialog...")
+        
+    def save_as_dialog(self):
+        print("saving file...")
+    
     def create_menu(self):
-        pass
+        menu = tk.Menu()
+        self.master.config(menu = menu)
+        file_menu = tk.Menu(menu, tearoff=0)
+        file_menu.add_command(label="Open", command=self.open_dialog)
+        file_menu.add_command(label="Save as", command=self.save_as_dialog)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.master.destroy)
+        menu.add_cascade(label="File", menu=file_menu)
+        
+        
     
     
     def set_display(self):
