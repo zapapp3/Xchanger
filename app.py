1a0d8af19f1307691526b99689ee9d794faa118f
zapapp3 <zapapp3@gmail.com>
2024-05-19 05:49:36 -0700

    add dynamic screen resize with a checkbutton and settings.json change on click

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pathlib
 import shutil
+import json
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
@@ -17,6 +18,7 @@ class MainFrame(ttk.Frame):
         
         # images and icons var
         self.logo_icon = ttk.PhotoImage(file="./images/x-logo2.png")
+        self.setting_icon = ttk.PhotoImage(file="./images/setting.png")
         
         # create form inputs
         # self.form()
@@ -37,6 +39,8 @@ class MainFrame(ttk.Frame):
         
         # action point
         self.saved_action = None
+        
+        
         
     
     def run_action(self):
@@ -65,14 +69,92 @@ class MainFrame(ttk.Frame):
         
         self.saved_action = (("folder", opened_folder))
         
+        
+        
+    def set_setting(self, to_set):
+        self.settings_container = ttk.Toplevel(title="Setting", iconphoto="./images/setting.png")
+        
+        # setting functions
+        def read_settings():
+            with open ("./prop_info/settings.json", "r") as settings_file:
+                settings_info = json.load(settings_file)
+                return settings_info
+            
+        def write_setings():
+            pass
+        
+        
+        def set_resizable(self):
+            if self.settings_screen_resizable:
+                self.settings_container.resizable(True, True)
+            else:
+                self.settings_container.resizable(False, False)
+        
+        def change_resizable():
+            state = resizable_state.get()
+            new_settings = read_settings()
+            
+            new_settings["settings"]["resizable"] = state and True or False
+            
+            with open(file="./prop_info/settings.json", mode="w") as settings_file:
+                json.dump(new_settings, settings_file)
+            
+            self.settings_screen_resizable = read_settings()["settings"]['resizable']
+            set_resizable(self)
+            
+            
+        self.settings_screen_resizable = read_settings()["settings"]['resizable']
+        set_resizable(self)
+        
+        
+        # self.settings_container = ttk.Toplevel(title="Setting", iconphoto="./images/setting.png")
+        # set_resizable(self)
+                
+        
+            
+        # display
+        width = round(0.35 * self.screen_width)
+        height = round(0.50 * self.screen_height)
+        x = round((self.screen_width - width) / 2)
+        y = round((self.screen_height - height) / 2)
+        self.settings_container.geometry("{}x{}+{}+{}".format(width, height, x, y))
+        style = self.settings_container.style
+        style.configure(".", font=("Calibri", 12))
+        
+        
+        
+        # adding conditional contents
+        resizable_state = ttk.IntVar()
+        resizable_state.set(self.settings_screen_resizable and 1)
+        if (to_set == "view"):
+            wrapper = ttk.Frame(master=self.settings_container, padding=20)
+            check = ttk.Checkbutton(
+                master=wrapper,
+                text="Window resizable",
+                variable=resizable_state,
+                command=change_resizable
+            )
+            
+            check.pack()
+            wrapper.pack(fill=BOTH, expand=True)
+            
+            
+        elif (to_set == "history"):
+            print("check history")
+        elif (to_set == "others"):
+            print("other settings")
+        else:
+            print("unknown settings.")
+        
     def view_settings(self):
-        pass
+        self.set_setting("view")
     
     def history_settings(self):
-        pass
+        self.set_setting("history")
     
     def other_settings(self):
-        pass
+        self.set_setting("others")
+        
     
     def create_file_menu(self):
         # creating menu
